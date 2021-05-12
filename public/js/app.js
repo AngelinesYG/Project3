c2b02b1652ba61be3d88c1246a634f27fdf7fcaf
Christopher Nugroho <Chrisnulis@gmail.com>
2021-05-12 14:49:49 -0400

    Commit

--- public/js/app.js
+++ public/js/app.js
@@ -80,9 +80,7 @@ deletePlace = (event) => {
     return (
 
     <div>
-
-      <section className="add-place">
-        <form onSubmit={this.handleSubmit} className="add-place">
+        <form className="form-group col-sm-6" onSubmit={this.handleSubmit}>
           <label htmlFor="name">Name</label>
           <input
             className="form-control"
@@ -127,12 +125,10 @@ deletePlace = (event) => {
             value={this.state.place.description}
           />
 
-          <input type="submit" id="Add Place" />
+          <input className="btn btn-success mb-5" type="submit" id="Add Place" />
 
         </form>
-      </section>
 
-      <section className="place-list">
         <ul>
           {this.state.place.map(place => {
             return (
@@ -143,12 +139,13 @@ deletePlace = (event) => {
                   <details>
                     <summary>Description</summary>
                       <p>City: {place.city}</p>
-                      <p>{place.description}</p>
+                      <p>Country: {place.country}</p>
+                      <p>Description: {place.description}</p>
                   </details>
 
 
                   <details>
-                    <summary>Edit / Delete</summary>
+                    <summary>Edit</summary>
                     <form id={place._id} onSubmit={this.state.updatePlace}>
 
                       <label htmlFor="name">Name</label><br/>
@@ -185,18 +182,16 @@ deletePlace = (event) => {
                         onChange={this.handleChange}
                       /><br/>
 
-                      <input type="submit" id="Submit Edits" />
-
+                      <input className="btn btn-primary mb-1" type="submit" id="Submit Edits" />
                     </form>
-                    <button value={place._id} onClick={this.deletePlace}>Delete</button>
-
                   </details>
+                  <button value={place._id} onClick={this.deletePlace} className="btn btn-danger">Delete</button>
+
                 </div>
               </li>
             )
           })}
         </ul>
-      </section>
     </div>
     )
   }
