2d2521f90fdb0adaee782fa5b908204d07d1f4eb
Angelines Yaport-Garcia <angelinesyg@gmail.com>
2021-05-12 11:15:21 -0400

    Completed app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,197 @@
 class App extends React.Component {
   state = {
     places: [],
-    image: '',
-    name: '',
-    city: '',
-    country:'',
-    description: ''
+    name: "",
+    image: "",
+    city: "",
+    country: "",
+    description: ""
   }
 
+/* ----- HANDLE CHANGE: ------  */
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
 
+/* ------ HANDLE SUBMIT: ------  */
 
-  ReactDOM.render(
-    <App />,
-    document.querySelector('main')
+  handleSubmit = (event) => {
+    event.preventDefault()
+    axios.post('/places', this.state).then(
+      (response) => {
+        this.setState({
+          places: response.data,
+          name: "",
+          image: "",
+          city: "",
+          country: "",
+          description: ""
+        })
+      }
+    )
+  }
+
+/* ------------- UPDATE PLANT -------------  */
+
+updatePlaces = (event) => {
+  event.preventDefault()
+  const id = event.target.id
+  console.log(event.target.id);
+  axios.put('/places/' + id, this.state).then(
+    (response) => {
+      this.setState({
+        places: response.data,
+        name: "",
+        image: "",
+        city: "",
+        country: "",
+        description: ""
+      })
+    }
   )
+}
+
+/* ------------- DELETE PLANT -------------  */
+
+deletePlaces = (event) => {
+  axios.delete('/places/' + event.target.value).then(
+    (response) => {
+      this.setState({
+        places: response.data
+      })
+    }
+  )
+}
+
+/* ----- MOUNT DATA ON LOAD FUNCTION: ------  */
+  componentDidMount = () => {
+    axios.get('/places').then(response => {
+      this.setState({
+        places: response.data
+      })
+    })
+  }
+/*  ------ END COMPONENT DID MOUNT -----  */
+
+  render = () => {
+    return <div>
+
+      <section className="add-places">
+        <form onSubmit={this.handleSubmit} className="add-place">
+        <h3>Add Places</h3>
+          <label htmlFor="name">Name</label><br/>
+          <input
+            type="text"
+            id="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+          /><br/>
+
+          <label htmlFor="image">Image</label><br/>
+          <input
+            type="text"
+            id="image"
+            onChange={this.handleChange}
+            value={this.state.image}
+          /><br/>
+
+          <label htmlFor="city">City</label><br/>
+          <input
+            type="text"
+            id="city"
+            onChange={this.handleChange}
+            value={this.state.city}
+          /><br/>
+
+          <label htmlFor="country">Country</label><br/>
+          <input
+            type="text"
+            id="country"
+            onChange={this.handleChange}
+            value={this.state.country}
+          /><br/>
+          <label htmlFor="description">Description</label><br/>
+          <input
+            type="text"
+            id="description"
+            onChange={this.handleChange}
+            value={this.state.description}
+          /><br/>
+
+          <input type="submit" id="Add Place" />
+
+        </form>
+      </section>
+
+      <section className="places-list">
+        <ul>
+          {this.state.places.map(plant => {
+            return (
+              <li key={places._id}>
+                <h2>{places.name}</h2>
+                <img src={places.image} alt={places.name} />
+                <div className="dropdowns">
+                  <details>
+                    <summary>Description</summary>
+                      <p>City: {places.city}</p>
+                      <p>{places.description}</p>
+                  </details>
+                  <details>
+                    <summary>Edit / Delete</summary>
+                    <form id={places._id} onSubmit={this.updatePlaces}>
+                      <label htmlFor="name">Name</label><br/>
+                      <input
+                        type="text"
+                        id="name"
+                        onChange={this.handleChange}
+                      /><br/>
+
+                      <label htmlFor="image">Image</label><br/>
+                      <input
+                        type="text"
+                        id="image"
+                        onChange={this.handleChange}
+                      /><br/>
+
+                      <label htmlFor="city">City</label><br/>
+                      <input
+                        type="text"
+                        id="city"
+                        onChange={this.handleChange}
+                      /><br/>
+
+                      <label htmlFor="country">Country</label><br/>
+                      <input
+                        type="text"
+                        id="country"
+                        onChange={this.handleChange}
+                      /><br/>
+                      <label htmlFor="description">Description</label><br/>
+                      <input
+                        type="text"
+                        id="description"
+                        onChange={this.handleChange}
+                      /><br/>
+
+                      <input type="submit" id="Submit Edits" />
+
+                    </form>
+                    <button value={places._id} onClick={this.deletePlaces}>Delete</button>
+                  </details>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </div>
+  }
+}
+
+ReactDOM.render(
+  <App></App>,
+  document.querySelector('main')
+)
